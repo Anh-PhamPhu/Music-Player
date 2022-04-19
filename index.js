ebb13b092dbc921243391c20f28c0c1fd3ec1760
anh.phamphu <anhpham.96.k@gmail.com>
2022-04-19 11:45:48 +0900

    Created html css

---
+++ index.js
@@ -0,0 +1,12 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const app = {
+    song : [
+
+    ],
+    start: function(){
+
+    }
+}
+app.start();
